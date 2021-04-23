d5dcb32bfe8251bb758f61cbd68bec0510d25b0b
張凱鈞-mac <>
2021-04-23 17:57:31 +0800

    all data

--- readcsv.go
+++ readcsv.go
@@ -11,7 +11,33 @@ import (
 )
 
 func main() {
-	file, err := os.Open("2020_fut.csv")
+	allFileName := []string{
+		"1998_fut.csv", "1999_fut.csv", "2000_fut.csv", "2001_fut.csv",
+		"2002_fut.csv", "2003_fut.csv", "2004_fut.csv", "2005_fut.csv",
+		"2006_fut.csv", "2007_fut.csv", "2008_fut.csv", "2009_fut.csv",
+		"2010_fut.csv", "2011_fut.csv", "2012_fut.csv", "2013_fut.csv",
+		"2014_fut.csv", "2015_fut.csv", "2016_fut.csv", "2017_fut.csv",
+		"2018_fut.csv", "2019_fut.csv", "2020_fut.csv",
+	}
+
+	for _, fileName := range allFileName {
+		fileName = "o_data/"+fileName
+		readCSV(fileName)
+	}
+
+}
+
+// func writeTxt(data string) {
+// 	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE //開啟檔案的選項
+// 	file, err := os.OpenFile("new.csv", options, os.FileMode(0600))
+// 	check(err)
+// 	_, err = fmt.Fprintln(file, data)
+// 	check(err)
+// 	err = file.Close()
+// }
+
+func readCSV(fileName string) {
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,18 +45,25 @@ func main() {
 	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE //開啟檔案的選項
 	file2, err := os.OpenFile("new.csv", options, os.FileMode(0600))
 	check(err)
-	i := 0
+	// i := 0
 	for scanner.Scan() {
-		if i == 0{
-			_, err = fmt.Fprintln(file2, scanner.Text())
-			check(err)
-			i++
-			continue
-		}
-
+		// if i < 0 {
+		// 	_, err = fmt.Fprintln(file2, scanner.Text())
+		// 	check(err)
+		// 	i++
+		// 	continue
+		// }
 		sli := strings.Split(scanner.Text(), ",")
 		if len(sli) > 1 {
-			if sli[1] == "MTX" && sli[17] == "一般" {
+			ok:=false
+			if(len(sli) < 18){
+				ok= true
+			}else{
+				if(sli[17] == "一般"){
+					ok=true
+				}
+			}
+			if sli[1] == "MTX" && ok {
 				if strings.Trim(sli[2], " ") == finalDay(sli[0]) { // 第三週的禮拜三結算，隔天要抓下個月的值
 					_, err = fmt.Fprintln(file2, scanner.Text())
 					check(err)
@@ -49,17 +82,7 @@ func main() {
 	if scanner.Err() != nil {
 		log.Fatal(scanner.Err())
 	}
-
 }
-
-// func writeTxt(data string) {
-// 	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE //開啟檔案的選項
-// 	file, err := os.OpenFile("new.csv", options, os.FileMode(0600))
-// 	check(err)
-// 	_, err = fmt.Fprintln(file, data)
-// 	check(err)
-// 	err = file.Close()
-// }
 
 func check(err error) {
 	if err != nil {
@@ -73,8 +96,8 @@ func finalDay(sli string) string {
 	month, _ := strconv.Atoi(a[1])
 	day, _ := strconv.Atoi(a[2])
 	b := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.Local)
-	c := b.Weekday()//每個月一號是禮拜幾
-	final := 0 //結算日
+	c := b.Weekday() //每個月一號是禮拜幾
+	final := 0       //結算日
 	if c == 0 {
 		final = 18
 	}
